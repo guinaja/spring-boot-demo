67b203601e427cfce56d0a95f2676d8aecc01d67
prayoonsuntornkiti@gmail.com <prayoonsuntornkiti@gmail.com>
2017-04-25 18:21:18 +0700

    initial

--- cloudserver/src/main/java/demo/CloudServerApplication.java
+++ cloudserver/src/main/java/demo/CloudServerApplication.java
@@ -3,28 +3,29 @@ package demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by prayoon-pc on 4/25/2017.
  */
+@EnableEurekaServer
 @SpringBootApplication
-@EnableDiscoveryClient
-@RestController
 public class CloudServerApplication {
 
     @Autowired
     DiscoveryClient client;
 
-    @RequestMapping("/")
-    public String hello() {
-        ServiceInstance localInstance = client.getLocalServiceInstance();
-        return "Hello World: "+ localInstance.getServiceId()+":"+localInstance.getHost()+":"+localInstance.getPort();
-    }
+//    @RequestMapping("/")
+//    public String hello() {
+//        ServiceInstance localInstance = client.getLocalServiceInstance();
+//        return "Hello World: "+ localInstance.getServiceId()+":"+localInstance.getHost()+":"+localInstance.getPort();
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(CloudServerApplication.class, args);
