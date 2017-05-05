f50b17d32c525e962c4bb8ca176786dc302f5174
prayoonsuntornkiti@gmail.com <prayoonsuntornkiti@gmail.com>
2017-05-05 17:51:33 +0700

    initial cloud config

---
+++ cloud-config-server/src/main/java/CloudConfigServerApplication.java
@@ -0,0 +1,14 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by prayoon-pc on 5/5/2017.
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class CloudConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigServerApplication.class, args);
+    }
+}
