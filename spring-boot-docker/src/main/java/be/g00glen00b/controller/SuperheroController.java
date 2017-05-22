7d859dbef5456688447f4a372182c866b7f62c5a
prayoonsuntornkiti@gmail.com <prayoonsuntornkiti@gmail.com>
2017-05-22 17:49:15 +0700

    add log / copy jar to docker folder

--- spring-boot-docker/src/main/java/be/g00glen00b/controller/SuperheroController.java
+++ spring-boot-docker/src/main/java/be/g00glen00b/controller/SuperheroController.java
@@ -1,11 +1,13 @@
 package be.g00glen00b.controller;
 
 import be.g00glen00b.repository.SuperheroRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 @RequestMapping("/superhero")
 public class SuperheroController {
@@ -14,6 +16,8 @@ public class SuperheroController {
 
     @RequestMapping
     public ModelAndView getSuperheroes() {
+        log.debug("debug invoke getSuperheroes");
+        log.info("info invoke getSuperheroes");
         return new ModelAndView("superheroes", "superheroes", repository.findAll());
     }
 }
